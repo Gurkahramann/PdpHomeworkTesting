8c6b8b2ce5609261d8ab975f5b3a6c8dbb5f6d9b
Gürkan Kahraman <100561567+Gurkahramann@users.noreply.github.com>
2024-03-07 23:31:35 +0300

    A File Testing

---
+++ A.java
@@ -0,0 +1,85 @@
+/**
+ * Bu bir Javadoc yorumudur.
+ * Bu sınıf örnek bir Java sınıfıdır ve belirli istatistiklere sahiptir.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * (Bu Javadoc yorumu toplamda 25 satır olacak şekilde ayarlanmıştır)
+ */
+public class A {
+    // Bu bir tek satırlık yorumdur.
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // (Bu bölümde toplam 20 satır yorum bulunmaktadır)
+    public void function1() {
+
+    }
+
+    public void function2() {
+
+    }
+    public void function3() {
+
+    }
+
+    public void function4() {
+
+    }
+    public void function5() {
+
+    }
+
+    public void function6() {
+
+    }
+    public void function7() {
+
+    }
+
+    public void function8() {
+
+    }
+    public void function9() {
+
+    }
+
+    public void function10() {
+
+    }
+
+
+}
+
