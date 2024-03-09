fd27df22ce31fe2cacb5bb05a53345795aecee8d
Gürkan Kahraman <100561567+Gurkahramann@users.noreply.github.com>
2024-03-09 22:17:52 +0300

    Update A.java

--- A.java
+++ A.java
@@ -1,85 +1,47 @@
-//public class A {
+package odev;
+import java.util.UUID;
+public class Motor {
+private String motorNo;
+private boolean calisiyor;
 /**
- * Bu bir Javadoc yorumudur.
- * Bu sınıf örnek bir Java sınıfıdır ve belirli istatistiklere sahiptir.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- * (Bu Javadoc yorumu toplamda 25 satır olacak şekilde ayarlanmıştır)
- */
-    // Bu bir tek satırlık yorumdur.
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    // (Bu bölümde toplam 20 satır yorum bulunmaktadır)
-    public void function1() {
-
-    }
-
-    public void function2() {
-
-    }
-    public void function3() {
-
-    }
-
-    public void function4() {
-
-    }
-    public void function5() {
-
-    }
-
-    public void function6() {
-
-    }
-    public void function7() {
-
-    }
-
-    public void function8() {
-
-    }
-    public void function9() {
-
-    }
-
-    public void function10() {
-
-    }
-
-
+* Varsayılan kurucu fonksiyon
+*/
+public Motor() {
+this.motorNo = UUID.randomUUID().toString();
+/* Başlangıçta false */calisiyor = false;
 }
-
+/**
+*
+* @param motorNo UUID olarak motor id
+* @return motor instance
+*/
+public Motor(String motorNo) {
+/*
+* Varolan bir motor no ile oluşturuluyor.
+*/
+this.motorNo = motorNo;
+calisiyor = false; // false yapılıyor
+}
+public void calistir() {
+/**
+* calisiyor true yapılıyor
+*/
+calisiyor = true;
+}
+/**
+* Motorun durdurulması //
+*/
+public void durdur() {
+calisiyor = false;
+}
+public String getMotorNo() {
+// motor no getir
+return motorNo;
+}
+@Override
+public String toString() {
+// durum belirlenmesi //
+String durum = calisiyor ? "Motor Çalışıyor." : "Motor Çalışmıyor";
+return durum;
+}
+}
