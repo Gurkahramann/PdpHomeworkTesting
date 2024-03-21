638a7ca3abf645e804359a6a268508b7a9d16745
Gürkan Kahraman <100561567+Gurkahramann@users.noreply.github.com>
2024-03-21 23:40:18 +0300

    Update A.java

--- A.java
+++ A.java
@@ -1,47 +1,28 @@
-package odev;
-import java.util.UUID;
-public class Motor {
-private String motorNo;
-private boolean calisiyor;
+package pkt;
+
 /**
-* Varsayılan kurucu fonksiyon
-*/
-public Motor() {
-this.motorNo = UUID.randomUUID().toString();
-/* Başlangıçta false */calisiyor = false;
-}
-/**
-*
-* @param motorNo UUID olarak motor id
-* @return motor instance
-*/
-public Motor(String motorNo) {
-/*
-* Varolan bir motor no ile oluşturuluyor.
-*/
-this.motorNo = motorNo;
-calisiyor = false; // false yapılıyor
-}
-public void calistir() {
-/**
-* calisiyor true yapılıyor
-*/
-calisiyor = true;
-}
-/**
-* Motorun durdurulması //
-*/
-public void durdur() {
-calisiyor = false;
-}
-public String getMotorNo() {
-// motor no getir
-return motorNo;
-}
-@Override
-public String toString() {
-// durum belirlenmesi //
-String durum = calisiyor ? "Motor Çalışıyor." : "Motor Çalışmıyor";
-return durum;
-}
+ * 
+ * @author MFA
+ *
+ */
+public class Atm {
+	/**
+	 * <p>
+	 *   Atm'den para çekme işlemi
+	 * </p>
+	 * @param kart ATM'ye verilen kart
+	 * @param sifre Kart şifresi
+	 * @param miktar Çekilecek para miktarı
+	 * @return Para çekme işleminin başarılımı geçtiğini döndürür.
+	 */
+	public boolean paraCek(IKart kart,String sifre, double miktar) {
+		if(!kart.girisKontrol(sifre)) return false;
+		return kart.getHesap().paraCek(miktar);
+	}
+	
+	public boolean paraYatir(IKart kart,String sifre, double miktar) {
+		// Şifre kontrolü
+		if(!kart.girisKontrol(sifre)) return false;
+		return kart.getHesap().paraYatir(miktar);
+	}
 }
